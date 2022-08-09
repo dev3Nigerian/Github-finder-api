f0d65b03ed20bc03d6df1170d3f05b53033c1c63
dev3Nigerian <faithfulodigbo@gmail.com>
2022-08-09 11:41:49 +0100

    initial

---
+++ github.js
@@ -0,0 +1,22 @@
+class gitHub {
+  constructor() {
+    this.client_id = '5b49d8f4fb01c5fc51c9';
+    this.client_secret = '7c101bbafe5436d028b8293b1aa4fcba604a2ed7';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc'
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
